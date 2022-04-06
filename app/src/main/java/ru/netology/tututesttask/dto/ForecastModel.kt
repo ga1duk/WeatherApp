f1327058234fdc48a540102e9b222948260a2759
ga1duk <gaidukdg@yandex.ru>
2022-04-06 15:14:33 +0300

    Icon with weather conditions is now вшdisplayed at WeatherFragment

--- app/src/main/java/ru/netology/tututesttask/dto/ForecastModel.kt
+++ app/src/main/java/ru/netology/tututesttask/dto/ForecastModel.kt
@@ -2,7 +2,15 @@ package ru.netology.tututesttask.dto
 
 data class ForecastModel(val fact: FactModel, val geo_object: GeoObject?)
 
-data class FactModel(val temp: Int)
+data class FactModel(
+    val temp: Int,
+    val icon: String,
+    val condition: String
+) {
+    fun getConditionIcon(): String {
+        return "https://yastatic.net/weather/i/icons/funky/dark/$icon.svg"
+    }
+}
 
 data class GeoObject(val locality: Locality)
 
--- app/src/main/java/ru/netology/tututesttask/ui/WeatherFragment.kt
+++ app/src/main/java/ru/netology/tututesttask/ui/WeatherFragment.kt
@@ -1,17 +1,21 @@
 package ru.netology.tututesttask.ui
 
+import android.graphics.drawable.PictureDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
 import ru.netology.tututesttask.R
 import ru.netology.tututesttask.databinding.FragmentWeatherBinding
 import ru.netology.tututesttask.ui.CityFragment.Companion.textArg
 import ru.netology.tututesttask.viewmodel.WeatherViewModel
 
 class WeatherFragment : Fragment() {
+    private var requestBuilder: RequestBuilder<PictureDrawable>? = null
     private val viewModel: WeatherViewModel by viewModels(ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
@@ -20,10 +24,14 @@ class WeatherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentWeatherBinding.inflate(inflater, container, false)
+        requestBuilder = Glide.with(this)
+            .`as`(PictureDrawable::class.java)
 
         viewModel.weatherData.observe(viewLifecycleOwner) { forecastModel ->
             binding.tvCity.text = forecastModel.geo_object?.locality?.name ?: arguments?.textArg
+            requestBuilder?.load(forecastModel.fact.getConditionIcon())?.into(binding.ivIcon)
             binding.tvTemp.text = "${forecastModel.fact.temp} ${resources.getString(R.string.degrees_by_celsius_text)}"
+            binding.tvCondition.text = forecastModel.fact.condition
         }
 
         return binding.root
