ae006af29a00b33ca965f41fb6a24893dfb86fee
ga1duk <gaidukdg@yandex.ru>
2022-04-07 15:34:11 +0300

    ActionBar was hidden. textView color was changed to white.TextView and background colors were moved to themes.xml

--- app/src/androidTest/java/ru/netology/myweatherapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/netology/myweatherapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.netology.tututesttask", appContext.packageName)
+        assertEquals("ru.netology.myweatherapp", appContext.packageName)
     }
 }
